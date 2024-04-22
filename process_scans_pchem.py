c066d77a33b1479c82aadd7dc60e81a17a15ba92
Sean Garrett-Roe <sgarrettroe@gmail.com>
2024-04-22 10:08:12 -0400

    style: Improve PEP8 conformance

--- process_scans_pchem.py
+++ process_scans_pchem.py
@@ -1,19 +1,18 @@
 import subprocess
 import sys
 import os
-#import glob
-from pathlib import Path #for sorting paths below (supercedes glob)
+from pathlib import Path  # for sorting paths below (supercedes glob)
 import logging
-import re #regular expressions (needed searching for bond angles in chemFigString)
-from string import Template #used in annotations
+import re  # regular expressions
+from string import Template  # used in annotations
+from subprocess import CompletedProcess
 
-#set up logger
+# set up logger
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-#logger.setLevel(logging.INFO)
 
 # path to gs >= 9.54
-gscmd = '/Users/SGR/Downloads/ghostscript-9.55.0/bin/gs'
+gs_cmd = '/Users/SGR/Downloads/ghostscript-9.55.0/bin/gs'
 
 # path to tessdata files
 TESSDATA_PREFIX = '/Users/SGR/Downloads/tessdata_fast-main/'
@@ -23,41 +22,34 @@ my_env = os.environ.copy()
 my_env["TESSDATA_PREFIX"] = TESSDATA_PREFIX
 
 # assessment list
-ASSLIST = ['10a', '1c', '2c', '3c', '4c', '5c', '6c', 
-           '7c', '8c', '9b']
+ASS_LIST = ['10a', '1c', '2c', '3c', '4c', '5c', '6c',
+            '7c', '8c', '9b']
 
-def cropcorner(pdfin) -> int:
+
+def crop_corner(pdf_in) -> CompletedProcess[bytes]:
     """Crop the corner of the pdf file to extract the assessment name"""
-    logging.info(f'\t...cropping {pdfin} to crop_{pdfin}')
-    #first try lower left of pdf (not scan)
-    #sizestr = '700x500'
-    #offsetstr = '-30 -25'
-
-    #second try lower left of scans
-    #sizestr = '700x500'
-    #offsetstr = '-45 -60'
-
-    #third try title
-    sizestr = '1700x750'
-    offsetstr = '-45 -675'
+    logging.info(f'\t...cropping {pdf_in} to crop_{pdf_in}')
+    # third try title
+    size_str = '1700x750'
+    offset_str = '-45 -675'
     
-    x = subprocess.run([gscmd,'-q','-o','crop_'+pdfin,'-sDEVICE=pdfwrite',f'-g{sizestr}','-c',f'"<</PageOffset [{offsetstr}]>> setpagedevice"','-f',pdfin])
+    x = subprocess.run([gs_cmd, '-q', '-o', 'crop_' + pdf_in, '-sDEVICE=pdfwrite', f'-g{size_str}', '-c', f'"<</PageOffset [{offset_str}]>> setpagedevice"', '-f', pdf_in])
     
     return x
 
-def ocrcorner(pdfin) -> int:
+
+def ocr_corner(pdf_in) -> CompletedProcess[bytes]:
     """run ocr on the cropped corner"""
-    logging.info(f'\t...running OCR on crop_{pdfin}')
-    x = subprocess.run([gscmd,'-q','-o','crop_'+pdfin+'-%03d.txt','--permit-file-read=$TESSDATA_PREFIX','-sDEVICE=ocr','-r600','-dDownScaleFactor=3','crop_'+pdfin],env=my_env)
+    logging.info(f'\t...running OCR on crop_{pdf_in}')
+    x = subprocess.run([gs_cmd, '-q', '-o', 'crop_' + pdf_in + '-%03d.txt', '--permit-file-read=$TESSDATA_PREFIX', '-sDEVICE=ocr', '-r600', '-dDownScaleFactor=3', 'crop_' + pdf_in], env=my_env)
     return x
 
-def buildpagelist3(pdfin)->dict:
+
+def build_page_list3(pdf_in) -> dict:
     logging.info('\t...build list of assessments')
 
-    #oldassnumber = '5.1' #first page
-    #oldpagenumber = 0
-    batch_of_files = sorted(Path('.').glob(f'crop_{pdfin}-*.txt'))
-    n_pages_expected_per_student = len(ASSLIST)
+    batch_of_files = sorted(Path('.').glob(f'crop_{pdf_in}-*.txt'))
+    n_pages_expected_per_student = len(ASS_LIST)
     n_pages_in_this_batch = len(batch_of_files)
     if n_pages_in_this_batch % n_pages_expected_per_student == 0:
         # if pages in batch is a multiple of the expected number
@@ -67,10 +59,11 @@ def buildpagelist3(pdfin)->dict:
         
     return assessment_page_dict
 
-def process_expected(batch_of_files)->dict:
+
+def process_expected(batch_of_files) -> dict:
 
     # (note int divide "//" )
-    n_pages_expected_per_student = len(ASSLIST)
+    n_pages_expected_per_student = len(ASS_LIST)
     n_pages_in_this_batch = len(batch_of_files)
     n_students_guess = n_pages_in_this_batch//n_pages_expected_per_student
     logging.debug(f'\t... got the expected number of pages \
@@ -80,99 +73,103 @@ def process_expected(batch_of_files)->dict:
     page_guess = [x + 1 + y * n_pages_expected_per_student
                   for y in range(n_students_guess) 
                   for x in range(n_pages_expected_per_student)]
-    ass_guess = ASSLIST * n_students_guess
-    expected_dict = dict(zip(page_guess,ass_guess))
+    ass_guess = ASS_LIST * n_students_guess
+    expected_dict = dict(zip(page_guess, ass_guess))
     
-    pageassdict = build_page_assessment_dict(batch_of_files)
+    page_ass_dict = build_page_assessment_dict(batch_of_files)
     
-    #look for errors
-    for key, val in pageassdict.items():
+    # look for errors
+    for key, val in page_ass_dict.items():
         if val is None:
             # we're guessing it is probably ok
             flag_ok_test1 = True
             flag_ok_test2 = True
-            if key-1 in pageassdict:
-                if pageassdict[key-1] != expected_dict[key-1]:
+            if key-1 in page_ass_dict:
+                if page_ass_dict[key-1] != expected_dict[key-1]:
                     flag_ok_test1 = False
-            if key+1 in pageassdict:
-                if pageassdict[key+1] != expected_dict[key+1]:
+            if key+1 in page_ass_dict:
+                if page_ass_dict[key+1] != expected_dict[key+1]:
                     flag_ok_test2 = False
             flag_ok = flag_ok_test1 and flag_ok_test2
             if flag_ok:
                 logging.debug(f'Guessing that page {key} is assessment {expected_dict[key]}')
-                pageassdict[key] = expected_dict[key]
+                page_ass_dict[key] = expected_dict[key]
             else:
-                pageassdict[key] = get_assessment_from_user(key,expected_dict[key])
+                page_ass_dict[key] = get_assessment_from_user(key, expected_dict[key])
                 
-    logging.debug(pageassdict)
-    flipped = flip_dictionary(pageassdict)
+    logging.debug(page_ass_dict)
+    flipped = flip_dictionary(page_ass_dict)
     logging.debug(flipped)
 
     return flipped
 
-def build_page_assessment_dict(batch_of_files)->dict:
+
+def build_page_assessment_dict(batch_of_files) -> dict:
     page_list = []
     ass_list = []
     for file in batch_of_files:
-        m = re.search(r'(?<=pdf-)\d+(?=\.txt)',str(file))
+        m = re.search(r'(?<=pdf-)\d+(?=\.txt)', str(file))
         if m:
-            pagenumber = int(m[0])
+            page_number = int(m[0])
         else:
             raise ValueError(f'Unable to extract the page number from {file}')
 
-        logging.debug(pagenumber)
+        logging.debug(page_number)
         
-        with open(file,'r') as fid:
-            ocrtext = fid.read()
-        logging.debug(ocrtext)
+        with open(file, 'r') as fid:
+            ocr_text = fid.read()
+        logging.debug(ocr_text)
 
-        assessment_string = get_assessment_from_ocr(ocrtext)            
-        page_list.append(pagenumber)
+        assessment_string = get_assessment_from_ocr(ocr_text)
+        page_list.append(page_number)
         ass_list.append(assessment_string)
-    #put them in a dictionary where pages are keys and assessments are values
-    pageassdict = dict(zip(page_list,ass_list))
-    return pageassdict
+    # put them in a dictionary where pages are keys and assessments are values
+    page_ass_dict = dict(zip(page_list, ass_list))
+    return page_ass_dict
 
-def get_assessment_from_user(page,expected_assessment=[])->str:
+
+def get_assessment_from_user(page, expected_assessment=None) -> str:
+    if expected_assessment is None:
+        expected_assessment = []
     print(f'Trouble on page {page}. Expected {expected_assessment}.')
     assessment_string = []
-    while assessment_string == []:
-        #val = input('What is the assessment for:\n' + ocrtext +'\n:\n') 
-        val = input('What is the assessment number:\n') 
-        if val in ASSLIST:
+    while not assessment_string:
+        val = input('What is the assessment number:\n')
+        if val in ASS_LIST:
             assessment_string = val
         else:
             print(f'{val} is not in the assessment list. Try again.')
     
     return assessment_string
 
-def get_assessment_from_ocr(ocrtext)->str:
+
+def get_assessment_from_ocr(ocr_text) -> str:
     # search for assessment by "Assessment 3.2a" template
     # seem to get a lot of extra .'s or \'s and 1 often reads as l
     # m as n, extra ` or ', e as a, . as :
     pattern = r'(?<=[mn][eaé]nt )[\dl]+[a-z]'
-    m = re.search(pattern,ocrtext)
+    m = re.search(pattern, ocr_text)
     if not m:
         # see if there is an extra . \ ' ` , 
         pattern = r"(?<=[mn][eaé]nt [\.\\\'\`,])[\dl]+[a-z]"
-        m = re.search(pattern,ocrtext)
+        m = re.search(pattern, ocr_text)
     if not m:
         # see if they are before the space
         pattern = r"(?<=[mn][eaé]nt[\.\\\'\`,] )[\dl]+[a-z]"
-        m = re.search(pattern,ocrtext)
+        m = re.search(pattern, ocr_text)
     if m:    
         # hopefully we found something
-        assnumber = m[0].replace('l','1').replace('L','1').replace('i','1').replace('I','1')
+        ass_number = m[0].replace('l', '1').replace('L', '1').replace('i', '1').replace('I', '1')
     else:
         # if not
-        assnumber = m #None if still no match
+        ass_number = m  # None if still no match
     
-    return assnumber
+    return ass_number
 
 
-def process_unexpected(batch_of_files)->dict:
+def process_unexpected(batch_of_files) -> dict:
     
-    n_pages_expected_per_student = len(ASSLIST)
+    n_pages_expected_per_student = len(ASS_LIST)
     n_pages_in_this_batch = len(batch_of_files)
     n_students_guess = n_pages_in_this_batch // n_pages_expected_per_student
     remainder = n_pages_in_this_batch % n_pages_expected_per_student
@@ -180,21 +177,21 @@ def process_unexpected(batch_of_files)->dict:
                      f'Got ({n_pages_in_this_batch}) for {n_students_guess}',
                      f'students with {remainder} left over'])
 
-    pageassdict = build_page_assessment_dict(batch_of_files)
+    page_ass_dict = build_page_assessment_dict(batch_of_files)
     
-    #look for errors
-    for key, val in pageassdict.items():
+    # look for errors
+    for key, val in page_ass_dict.items():
         if val is None:
-            pageassdict[key] = get_assessment_from_user(key)
+            page_ass_dict[key] = get_assessment_from_user(key)
 
-    logging.debug(pageassdict)
-    flipped = flip_dictionary(pageassdict)
+    logging.debug(page_ass_dict)
+    flipped = flip_dictionary(page_ass_dict)
     logging.debug(flipped)
 
     return flipped
 
     
-def flip_dictionary(d)->dict:
+def flip_dictionary(d) -> dict:
     flipped = {}
     for key, value in d.items():
         if value not in flipped:
@@ -203,25 +200,32 @@ def flip_dictionary(d)->dict:
             flipped[value].append(key)
     return flipped
 
-def processAssessment(pdfin,assessmentdict)->int:
-    logging.info('\t...extracting pages')
-    for key in assessmentdict:
-        assName = 'A'+key+'_'+pdfin
-        logging.debug(assName)
-        value = assessmentdict[key]
-        #must be in increasing order so sort, then make strings, then combine with comma sep
-        value.sort() #sorts in place        
-        tmp = [str(num) for num in value]
-        pagelist = ','.join(tmp)
-        logging.debug('Page list = ' + pagelist)
-        logging.debug(' '.join([gscmd,'-q','-o',assName,'-sPageList='+pagelist,'-sDEVICE=pdfwrite',pdfin]))
-        x = subprocess.run([gscmd,'-q','-o',assName,'-sPageList='+pagelist,'-sDEVICE=pdfwrite',pdfin])
-        
-        x = add_annotations_to_pdf(pdfin,assName,key,value)
 
+def process_assessment(pdf_in, assessment_dict) -> (
+        CompletedProcess[bytes] | None):
+    logging.info('\t...extracting pages')
+    x = None
+    for key in assessment_dict:
+        ass_name = 'A' + key + '_' + pdf_in
+        logging.debug(ass_name)
+        value = assessment_dict[key]
+        # must be in increasing order so sort, then make strings,
+        # then combine with comma sep
+        value.sort()  # sorts in place
+        tmp = [str(num) for num in value]
+        page_list = ','.join(tmp)
+        logging.debug('Page list = ' + page_list)
+        logging.debug(' '.join([gs_cmd, '-q', '-o', ass_name, '-sPageList=' + page_list, '-sDEVICE=pdfwrite', pdf_in]))
+        x = subprocess.run([gs_cmd, '-q', '-o', ass_name, '-sPageList=' + page_list, '-sDEVICE=pdfwrite', pdf_in])
+        x.check_returncode()
+
+        x = add_annotations_to_pdf(pdf_in, ass_name, key, value)
+        x.check_returncode()
     return x
 
-def add_annotations_to_pdf(pdfin,file_name_in,assessment_name,original_page_list)->int:
+
+def add_annotations_to_pdf(pdf_in, file_name_in, assessment_name,
+                           original_page_list) -> CompletedProcess[bytes]:
 
     pdfmarks_file_name = 'pdfmarks.txt'
     template_string = r'''[
@@ -235,34 +239,33 @@ def add_annotations_to_pdf(pdfin,file_name_in,assessment_name,original_page_list
 
 '''    
     file_name_out = file_name_in.rstrip('.pdf') + '_annotated' + '.pdf'
-    d = {'pdfin':pdfin,'assessment_name':assessment_name}
+    d = {'pdfin': pdf_in, 'assessment_name': assessment_name}
     count = 0
-    with open(pdfmarks_file_name,"w") as pdfmarks_file:
+    with open(pdfmarks_file_name, "w") as pdfmarks_file:
         for orig_page in original_page_list:
             count += 1
             d['this_page'] = count
             d['original_page'] = orig_page
             pdfmarks_file.write(Template(template_string).safe_substitute(d))
             
-        #gs -dBATCH -dNOPAUSE -dQUIET -sDEVICE=pdfwrite -sOutputFile=annotated.pdf  pdfmarks.txt original.pdf
-    x = subprocess.run([gscmd,'-dBATCH','-dNOPAUSE','-dQUIET', 
-                        '-sDEVICE=pdfwrite',f'-sOutputFile={file_name_out}',
+    x = subprocess.run([gs_cmd, '-dBATCH', '-dNOPAUSE', '-dQUIET',
+                        '-sDEVICE=pdfwrite', f'-sOutputFile={file_name_out}',
                         pdfmarks_file_name, file_name_in])
     
     return x
 
 
-def cleanup(pdfin):
+def cleanup(pdf_in):
     logging.info('\t...cleaning up')
 
-    dirdict = {}
-    dirdict['originals'] =  Path('./originals')
-    #dirdict['processed'] =  Path('./processed')
-    dirdict['cropped'] =  Path('./cropped')
-    dirdict['ocrtxt'] = Path('./ocrtxt')
-    dirdict['split'] = Path('./split')
+    dir_dict = {
+        'originals': Path('./originals'),
+        'cropped': Path('./cropped'),
+        'ocrtxt': Path('./ocrtxt'),
+        'split': Path('./split')
+    }
 
-    for p in dirdict.values():
+    for p in dir_dict.values():
         if p.is_dir():
             logging.debug(f'\t...found dir {p}')
         else:
@@ -270,45 +273,44 @@ def cleanup(pdfin):
             p.mkdir()
             
     # remove all temp files
-    for file in sorted(Path('.').glob(f'crop_{pdfin}-*.txt')):
-        #file.unlink() 
-        destination = dirdict['ocrtxt'] / file
+    for file in sorted(Path('.').glob(f'crop_{pdf_in}-*.txt')):
+        destination = dir_dict['ocrtxt'] / file
         file.replace(destination)
-    for file in sorted(Path('.').glob(f'A*_{pdfin}')):
-        #file.unlink() 
-        destination = dirdict['split'] / file
+    for file in sorted(Path('.').glob(f'A*_{pdf_in}')):
+        destination = dir_dict['split'] / file
         file.replace(destination)
-        
-    
+
     # move processed pdf to originals directory
-    source = Path(pdfin)
-    destination = dirdict['originals'] / pdfin
+    source = Path(pdf_in)
+    destination = dir_dict['originals'] / pdf_in
     source.replace(destination)
 
-    #move cropped pages to processed directory
-    for file in sorted(Path('.').glob(f'crop_{pdfin}')):
-        destination = dirdict['cropped'] / file
+    # move cropped pages to processed directory
+    for file in sorted(Path('.').glob(f'crop_{pdf_in}')):
+        destination = dir_dict['cropped'] / file
         file.replace(destination)
 
-def summarize(pdfin):
-    #https://stackoverflow.com/questions/4826485/ghostscript-pdf-total-pages
-    x = subprocess.run(['./summarize.sh',pdfin],stdout=subprocess.PIPE)
+
+def summarize(pdf_in):
+    # https://stackoverflow.com/questions/4826485/ghostscript-pdf-total-pages
+    x = subprocess.run(['./summarize.sh', pdf_in], stdout=subprocess.PIPE)
     logging.info(x.stdout.decode('utf-8'))
     return x    
     
 
-def main(item) -> None:
+def main(this_item) -> None:
             
-    logging.info(f'Processing {item}:')
-    summarize(item)
-    cropcorner(item)
-    ocrcorner(item)
-    assessmentdict = buildpagelist3(item)
-    processAssessment(item,assessmentdict)
-    cleanup(item)
+    logging.info(f'Processing {this_item}:')
+    summarize(this_item)
+    crop_corner(this_item)
+    ocr_corner(this_item)
+    assessment_dict = build_page_list3(this_item)
+    process_assessment(this_item, assessment_dict)
+    cleanup(this_item)
     logging.info('\t...done')
     
     return None
+
 
 if __name__ == '__main__':
     """Process pdf scans to separate assessment files"""
